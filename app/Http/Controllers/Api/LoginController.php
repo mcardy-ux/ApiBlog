eb862d37db5136deef7f0c7e35e22aedd3527f85
mcardi-ux-dev <juanchodavidcardenas@outlook.com>
2021-07-30 12:24:25 -0500

    Se ha modificado el sistema de login en la api

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -8,26 +8,32 @@ use App\Model\User;
 use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
 {
-    public function login(Request $request){
-        $validate=$this->validateLogin($request);
 
-        if (Auth::attemp($request->only('email','password'))) {
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        if (Auth::attempt($request->only('email', 'password'))) {
             return response()->json([
-                'token'=> $request->user()->createToken($request->name)->plainTextToken,
-                'message' => 'success'
+                'token' => $request->user()->createToken($request->name)->plainTextToken,
+                'message' => 'Success'
             ]);
-        }else{
-            return response()->json([
-                'message' => 'Unauthorized'
-            ],401); 
         }
 
+        return response()->json([
+            'message' => 'Unauthorized'
+        ], 401);
     }
-    public function validateLogin(Request $request){
-        return   $request->validate([
-            'email' =>'requerid|email',
-            'password' =>'requerid',
-            'name' =>'requerid',
+
+    public function validateLogin(Request $request)
+    {
+        return $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'name' => 'required'
         ]);
     }
 }
+
+
